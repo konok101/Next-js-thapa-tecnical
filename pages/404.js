f5eae180efa99c4fb08e6143e89aaf14bbbb026a
konok101 <rockykonok@gmail.com>
2022-10-10 21:37:01 +0600

    error page use settimeout with useeffect for auto backe home page after 3sec

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Erpage = () => {
     const router = useRouter();
+
+    const handleHomePage = () => {
+
+        router.push("/")
+    };
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [])
+
+
     return (
         <div>
             <h1>rrr error page</h1>
             {/*     <Link href='/'><a>go to home page</a></Link> */}
-            <a onClick={() => router.push('/')}>go to home page</a>
+            {/*    <a onClick={() => router.push('/')}>go to home page</a> */}
+            <a onClick={handleHomePage}>go to home page</a>
         </div>
     );
 };
